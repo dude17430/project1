aad2b99406010ac53baa3fe35ce0785410397c78
hellpowder <dustyolekh@gmail.com>
2015-12-18 13:51:33 -0500

    tower two now checks if an enemy is within a 100px radius

--- src/StratObject/TowerTwo.java
+++ src/StratObject/TowerTwo.java
@@ -15,7 +15,6 @@ public class TowerTwo {
 
 
     public TowerTwo(int x, int y, StratUtil su){
-
         this.x = x;
         this.y = y;
         this.su = su;
@@ -24,30 +23,21 @@ public class TowerTwo {
     }
 
     public void update(){
-
-
         if(su.getEnemyList().size()>0 && checkForEnemies()){
-
-
-
-
             if(System.currentTimeMillis()-lastShot>1000){
                 lastShot = System.currentTimeMillis();
                 su.newProjectile("t2",x+30,y+30);
             }
         }
-
     }
 
     public boolean checkForEnemies(){  //checks to see if enemy is within a towers radius
-
         if(!(su.getEnemyList().isEmpty())) { //if enemy list is not emepty
-
             for (int i = 0; i < su.getEnemyList().size(); i++) {
                 double dx = (su.getEnemyList().get(i).getX() - getX());//gets the x distance between the tower and enemy
                 double dy = (su.getEnemyList().get(i).getY() - getY());//gets the y distance between the tower and enemy
-                double distance = Math.sqrt((dx*dx)+(dy*dy));
-                if (distance < checkRadius + su.getEnemyList().get(i).getRadius()){
+                double distance = Math.sqrt((dx*dx)+(dy*dy)); //gets the absolute distance between the tower and enemy
+                if (distance < checkRadius + su.getEnemyList().get(i).getRadius()){//checks if the radius to be checked plus the enemies radius is greater than the absolute distance between the enemy and tower
                     return true;
                 }
             }
@@ -67,9 +57,8 @@ public class TowerTwo {
     public int getY() {
         return y;
     }
-
     public int getCheckRadius(){
         return checkRadius;
-    } //Right here!!--------------------
+    }
 }
 
